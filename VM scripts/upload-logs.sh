620c14bb9cbe8deb23a174f1ada238a64129c82b
oozpay <134742811+oozpay@users.noreply.github.com>
2025-05-25 17:19:24 +0300

    Update upload-logs.sh

--- VM scripts/upload-logs.sh
+++ VM scripts/upload-logs.sh
@@ -4,6 +4,8 @@ FILE=~/message-logger/messages.log
 BUCKET=[BUCKET_NAME] # insert actual bucket name here
 OBJECT=messages.log
 
+curl -X DELETE "https://storage.googleapis.com/$BUCKET/$OBJECT"
+
 curl -X PUT --data-binary @"$FILE" \
   -H "Content-Type: text/plain" \
   "https://storage.googleapis.com/$BUCKET/$OBJECT"
